183d37eef510c36f79a7d8d8f9cf60fee58eff39
NoorShinwari <nanoshin746@gmail.com>
2020-02-08 22:05:46 +0000

    learning javascript from scratch

---
+++ js-basics/index.js
@@ -0,0 +1,65 @@
+/*
+let name = "Noor";
+console.log(name);
+
+//Cannot be a reserved keyword
+//should be meaningful
+// Cannot start with a number (1name)
+// cannot contain a space or hypen (-)
+
+let firstName = "Noor"; //camelCase.
+let lastName = "Ullah"; */
+
+/*
+let interestRate = 0.3; // if you want to reassign a value, use variable
+interestRate = 1;
+console.log(interestRate);*/
+
+/*const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);*/
+// it will give an error because
+// we cannot reassign a constant
+/*
+let name = "Noor"; // String Literal
+let age = 30; // Number Literal
+let isApproved = true; // Boolean Literal "can be true or false"
+let firstName = undefied;
+let selectedColor = null; //when we wana clear the value of a variable
+*/
+/* Object
+let person = { //we call this syntex is called object literal
+    name: "Noor",
+    age: 29
+};
+// Dot Naotation
+person.name = "Shinwari"
+// Bracket Notation
+person["name"] = "Khan"
+console.log(person);
+*/
+/* Array
+let selectedColors = ["red","blue"]; // Array Literal
+//selectedColors[2] = "green"; we can also make it a number " look at line 46"
+//console.log(selectedColors[0]); // Each element has an index and that determine position of that element
+// if you want to access an element in array we use this index.
+selectedColors[2] = "1";
+console.log(selectedColors.length);
+//technically array is an object.
+*/
+/*
+//Function
+//performing a task
+function greet(name,lastName) {
+    console.log("Hello " + name + " "+ lastName);
+}// no semicolons
+greet("John","Smith");
+
+// Calculating a value
+function square(number) {
+    return number * number;
+}
+// Either 
+// let number = square(2);
+console.log(square(2)); // there are two functions in this code, one is console log and other is square.
+*/
