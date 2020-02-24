4b9eb67a23c80f1635336c58ce7bd74eb311d2aa
NoorShinwari <nanoshin746@gmail.com>
2020-02-24 13:24:49 +0100

    update

--- arrays/dataTypes.js
+++ arrays/dataTypes.js
@@ -12,5 +12,36 @@ camelize("list-style-image") == 'listStyleImage';
 camelize("-webkit-transition") == 'WebkitTransition';
 P.S. Hint: use split to split the string into an array, transform it and join back.
 */
+// let str = 'Hello';
+// let str2 = str.toUpperCase(str);
+// console.log(str2);
 
-function camelize(str) {}
+// str.test = 5;
+
+// console.log(str.test);
+
+
+Sum numbers from the visitor
+importance: 5
+Create a script that prompts the visitor to enter two numbers and then shows their sum.
+
+Run the demo
+
+P.S. There is a gotcha with types.
+
+function sum(a,b) {
+    this.a = +prompt("Enter 1st number?"),
+    this.b = +prompt("Enter second number?"),
+    return this.a + this.b
+}
+
+
+
+function sum(a,b) {
+    
+    this.a = +prompt("Enter 1st number?")
+    this.b = +prompt("Enter second number?")
+    let sum = this.a + this.b
+    alert(sum)
+}
+sum()
