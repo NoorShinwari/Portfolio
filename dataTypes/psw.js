731e84f06571be9ba34631a92d4c7b8ea32b604f
NoorShinwari <nanoshin746@gmail.com>
2020-03-20 21:40:36 +0100

    react and js

--- dataTypes/psw.js
+++ dataTypes/psw.js
@@ -1,4 +1,3 @@
-let psw = 'Hello Passw0rd';
 function checkPsw(psw) {
   var signal = [];
   var lengthPsw = psw.length;
@@ -12,6 +11,7 @@ function checkPsw(psw) {
   if (copyPsw2.length == copyPsw.length) signal.push('Almost one digit');
   if (copyPsw.replace(/ /, ''.length == 0))
     signal.push('Almost one special character');
+  console.log(copyPsw.replace(/ /g, ''));
   return signal.join('\n');
 }
 console.log(checkPsw(process.argv[2]));
