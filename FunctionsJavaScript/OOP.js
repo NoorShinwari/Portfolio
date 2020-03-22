541cf42f6a733764c57e3bcabd3a45c7f8dbe50a
NoorShinwari <nanoshin746@gmail.com>
2020-03-23 00:49:54 +0100

    oop using constuctor

--- FunctionsJavaScript/OOP.js
+++ FunctionsJavaScript/OOP.js
@@ -70,3 +70,71 @@ function Person(first, last, age, gender, interests) {
 }
 
 let person1 = new Person('Bob', 'Smith', 32, 'male', ['music', 'skiing']);
+////////////////////////////////////////////////////////////////////
+
+// Now changing the above function pronoun,,,,,,,
+
+function Person(first, last, age, gender, interests) {
+  this.name = {
+    first: first,
+    last: last
+  };
+  this.age = age;
+  this.gender = gender;
+  this.interests = interests;
+  this.bio = function() {
+    let string =
+      this.name.first +
+      ' ' +
+      this.name.last +
+      ' is ' +
+      this.age +
+      ' years old. He likes ' +
+      this.interests[0] +
+      ' and ' +
+      this.interests[1] +
+      '.';
+    let pronoun;
+    if (
+      this.gender === 'male' ||
+      this.gender === 'Male' ||
+      this.gender === 'm' ||
+      this.gender === 'M'
+    ) {
+      pronoun = 'He likes ';
+    } else if (
+      this.gender === 'female' ||
+      this.gender === 'Female' ||
+      this.gender === 'f' ||
+      this.gender === 'F'
+    ) {
+      pronoun = 'She likes ';
+    } else {
+      pronoun = 'They like ';
+    }
+    string += pronoun;
+    if (this.interests.length === 1) {
+      string += this.interests[0] + '.';
+    } else if (this.interests.length === 2) {
+      string += this.interests[0] + ' and ' + this.interests[1] + '.';
+    } else {
+      for (let i = 0; i < this.interests.length; i++) {
+        if (i === this.interests.length - 1) {
+          sring += this.interests[i] + ', ';
+        } else {
+          string += this.interests[i] + ', ';
+        }
+      }
+    }
+    alert(string);
+  };
+  this.greeting = function() {
+    alert("Hi! I'm " + this.name.first + '.');
+  };
+}
+
+let person1 = new Person('Tammi', 'Smith', 32, 'neutral', [
+  'music',
+  'skiing',
+  'kickboxing'
+]);
