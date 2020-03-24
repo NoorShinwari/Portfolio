c606132ede74fcfbc51e9410ff04e5f77d0c5268
NoorShinwari <nanoshin746@gmail.com>
2020-03-24 21:09:17 +0100

    New excises on setTime...

--- FunctionsJavaScript/scheduling.js
+++ FunctionsJavaScript/scheduling.js
@@ -45,3 +45,82 @@ let i = 1;
 setInterval(function() {
   func(i++);
 }, 100);
+
+/////////////////////////////////////////////////////77
+//ZERO DELAY setTimeout////////////////////
+
+let start = Date.now();
+let times = [];
+
+setTimeout(function run() {
+  times.push(Date.now() - start); // remember delay from the previous call
+
+  if (start + 100 < Date.now()) alert(times);
+  // show the delays after 100ms
+  else setTimeout(run); // else re-schedule
+});
+
+///////////////////////////////////////////////////////////////////////////////////////7777
+//77777777777777777777777777777777777777777777777777777777777777777777777777777777777777777
+
+/*Output every second
+importance: 5
+Write a function printNumbers(from, to) that outputs a number every second, starting from from and ending with to.
+
+Make two variants of the solution.
+
+Using setInterval.
+Using nested setTimeout.*/
+
+function printNumbers(from, to) {
+  let current = from;
+
+  let timerId = setInterval(function() {
+    alert(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+/////////////////USING NESTED setTimeout
+function printNumbers(from, to) {
+  let current = from;
+
+  setTimeout(function go() {
+    alert(current);
+    if (current < to) {
+      setTimeout(go, 1000);
+    }
+    current++;
+  }, 1000);
+}
+//If we also want the function to run immediately, then we can add an additional call on a separate line, like this:
+
+function printNumbers(from, to) {
+  let current = from;
+
+  function go() {
+    alert(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }
+
+  go();
+  let timerId = setInterval(go, 1000);
+}
+
+A; /*ny setTimeout will run only after the current code has finished.
+
+The i will be the last one: 100000000.*/
+
+let i = 0;
+
+setTimeout(() => alert(i), 100); // 100000000
+
+// assume that the time to execute this function is >100ms
+for (let j = 0; j < 100000000; j++) {
+  i++;
+}
