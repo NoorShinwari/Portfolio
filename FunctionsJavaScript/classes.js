518c8b76501c03de5047e175dd1dd98c03740334
NoorShinwari <nanoshin746@gmail.com>
2020-03-25 14:35:55 +0100

    some corrections

--- FunctionsJavaScript/classes.js
+++ FunctionsJavaScript/classes.js
@@ -90,60 +90,57 @@ class User {
 new User().sayHi();
 
 /////////////////////////CLASS PROPERTIES////////////////////////
-class MyClass {
-    prop = value; // property
-  
-    constructor(...) { // constructor
-      // ...
-    }
-  
-    method(...) {} // method
-  
-    get something(...) {} // getter method
-    set something(...) {} // setter method
-  
-    [Symbol.iterator]() {} // method with computed name (symbol here)
+/////////////Example/////////////////////
+class Clock {
+  constructor({ template }) {
+    this.template = template;
+  }
+
+  render() {
+    let date = new Date();
+
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+
+    console.log(output);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), 1000);
+  }
+}
+
+let clock = new Clock({ template: 'h:m:s' });
+clock.start();
+
+/*class MyClass {
+  prop = value; // property
+
+  constructor(...) { // constructor
     // ...
   }
 
-  /////////////////////////////////////////////////////////////////
-  ////////////////EXERCISE/////////////////////////////////
-  
-class Clock {
-    constructor({ template }) {
-      this.template = template;
-    }
-  
-    render() {
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      let output = this.template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
-    }
-  
-    stop() {
-      clearInterval(this.timer);
-    }
-  
-    start() {
-      this.render();
-      this.timer = setInterval(() => this.render(), 1000);
-    }
-  }
-  
-  
-  let clock = new Clock({template: 'h:m:s'});
-  clock.start();
+  method(...) {} // method
+
+  get something(...) {} // getter method
+  set something(...) {} // setter method
+
+  [Symbol.iterator]() {} // method with computed name (symbol here)
+  // ...
+} */
