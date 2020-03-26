8188302c990ae3bfecfd81d5daac55f3a0134e0b
NoorShinwari <nanoshin746@gmail.com>
2020-03-26 23:32:28 +0100

    exercise

---
+++ Errors/errorTask.js
@@ -0,0 +1,19 @@
+// // compare the two code fragments:
+// // the first one uses finally to execute the code after try..catcg:
+
+// try {
+//   work work
+// } catch (e) {
+//   handle errors
+// } finally {
+//   cleanup the working space
+// }
+// // The Second fragment pts the cleaning right agter try ... catch:
+
+// try {
+//   work work
+// } catch (e) {
+//   handle errors
+// }
+
+// cleaning up the working space
