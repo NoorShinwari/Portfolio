bb9636d8761f8caaa43efcf2a85bf83610e76949
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:44:33 +0100

    multiplier table

--- guessGames/table2.js
+++ guessGames/table2.js
@@ -1,25 +1,21 @@
 let multiplier;
 let num;
-let result
-let line='';
+let result;
+let line = '';
 
-for(multiplier=1; multiplier<=10; multiplier++)
-{
+for (multiplier = 1; multiplier <= 10; multiplier++) {
+  for (num = 1; num <= 10; num++) {
+    result = num * multiplier;
+    line += result + '\t';
 
-    for (num = 1; num<=10; num++) { 
-    
-        result=num*multiplier;
-        line+=result+'\t';
-   
-        if(num==10) {
-        line+=('\n')
-        }
-    } 
-    
-     
-    console.log('\n')
+    if (num == 10) {
+      line += '\n';
+    }
+  }
 
-    console.log(line);     
+  console.log('\n');
 
+  console.log(line);
 }
+//
 // Explained in table5.js also see the comments in table3.js.
