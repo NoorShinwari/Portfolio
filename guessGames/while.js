1291e9ffdc8ff6e38f12be270ab6f069f2a4f144
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:51:34 +0100

    guess game

--- guessGames/while.js
+++ guessGames/while.js
@@ -1,15 +1,16 @@
+//Guess game
 const numbr = Math.floor(Math.random() * 10 + 1);
-let running = true
+let running = true;
 
-while(running) {
-    let guess = prompt("Enter numbr")
-    if (guess == numbr) {
-        alert("Congratulation, " + numbr + " was the correct number")
-        running = false
-    } else if (guess == "exit") {
-        alert("Bye bye")
-        running = false
-    } else {
-        alert(guess + " is the wrong number, try again")
-    }
-} 
+while (running) {
+  let guess = prompt('Enter numbr');
+  if (guess == numbr) {
+    alert('Congratulation, ' + numbr + ' was the correct number');
+    running = false;
+  } else if (guess == 'exit') {
+    alert('Bye bye');
+    running = false;
+  } else {
+    alert(guess + ' is the wrong number, try again');
+  }
+}
