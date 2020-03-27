be17857be114e4579fee66620883c8452597512b
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:43:33 +0100

    prime number exercise failed....

--- guessGames/table.js
+++ guessGames/table.js
@@ -1,33 +1,24 @@
 let i; //  numbers in x direction
 let j; // numbers in y direction
-let prod// x * y = product
-let strng = ""; // we put them in a horizontal line.
+let prod; // x * y = product
+let strng = ''; // we put them in a horizontal line.
 
-for ( j = 1; j <= 10; j++){ 
-    
-    for (i = 1; i <=10; i++) { 
-        
-       prod = j * i ;
-       strng +=  prod + "";
-       
-        
-        
-       while (strng.length<3){
-            strng = " "+strng;
-            
-        }
-          strng += '\t'
-            
-           if (i == 10){
-               strng+='\n';
-           }
+for (j = 1; j <= 10; j++) {
+  for (i = 1; i <= 10; i++) {
+    prod = j * i;
+    strng += prod + '';
 
+    while (strng.length < 3) {
+      strng = ' ' + strng;
+    }
+    strng += '\t';
 
-     
-        } 
-        console.log(strng);
-    
+    if (i == 10) {
+      strng += '\n';
+    }
+  }
+  console.log(strng);
 }
+//
 // left the comments in table3.js. I failed here miserably, In the future, if I see this, and if will be able to, I will
 // do it....
-
