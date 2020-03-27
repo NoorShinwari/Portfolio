81d8a5c93bee3e692744b3d0ddc1ec870b8abe35
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:19:15 +0100

    many triangles with many for loops

--- functions/triangle.js
+++ functions/triangle.js
@@ -1,49 +1,68 @@
+//Many triangles with for loops
 function triAngle(height) {
-    for (let i = 0; i <= height; i++) {
-        let row = "";
-        for (let j = 0; j <= (height-i); j++) {
-            row += "*";
-        }for (let k = 0; k <= i; k++) {
-            row += "&";
-        }for (let l = 0; l <= i; l++) {
-            row += "&";
-        }for (let m = 0; m <= (height-i); m++) {
-            row += "*";
-        }
-        for (let j = 0; j <= (height-i); j++) {
-            row += "*";
-        }for (let k = 0; k <= i; k++) {
-            row += "&";
-        }for (let l = 0; l <= i; l++) {
-            row += "&";
-        }for (let m = 0; m <= (height-i); m++) {
-            row += "*";
-        }for (let j = 0; j <= (height-i); j++) {
-            row += "*";
-        }for (let k = 0; k <= i; k++) {
-            row += "&";
-        }for (let l = 0; l <= i; l++) {
-            row += "&";
-        }for (let m = 0; m <= (height-i); m++) {
-            row += "*";
-        }for (let j = 0; j <= (height-i); j++) {
-            row += "*";
-        }for (let k = 0; k <= i; k++) {
-            row += "&";
-        }for (let l = 0; l <= i; l++) {
-            row += "&";
-        }for (let m = 0; m <= (height-i); m++) {
-            row += "*";
-        }for (let j = 0; j <= (height-i); j++) {
-            row += "*";
-        }for (let k = 0; k <= i; k++) {
-            row += "&";
-        }for (let l = 0; l <= i; l++) {
-            row += "&";
-        }for (let m = 0; m <= (height-i); m++) {
-            row += "*";
-        }
-        console.log(row)
+  for (let i = 0; i <= height; i++) {
+    let row = '';
+    for (let j = 0; j <= height - i; j++) {
+      row += '*';
     }
+    for (let k = 0; k <= i; k++) {
+      row += '&';
+    }
+    for (let l = 0; l <= i; l++) {
+      row += '&';
+    }
+    for (let m = 0; m <= height - i; m++) {
+      row += '*';
+    }
+    for (let j = 0; j <= height - i; j++) {
+      row += '*';
+    }
+    for (let k = 0; k <= i; k++) {
+      row += '&';
+    }
+    for (let l = 0; l <= i; l++) {
+      row += '&';
+    }
+    for (let m = 0; m <= height - i; m++) {
+      row += '*';
+    }
+    for (let j = 0; j <= height - i; j++) {
+      row += '*';
+    }
+    for (let k = 0; k <= i; k++) {
+      row += '&';
+    }
+    for (let l = 0; l <= i; l++) {
+      row += '&';
+    }
+    for (let m = 0; m <= height - i; m++) {
+      row += '*';
+    }
+    for (let j = 0; j <= height - i; j++) {
+      row += '*';
+    }
+    for (let k = 0; k <= i; k++) {
+      row += '&';
+    }
+    for (let l = 0; l <= i; l++) {
+      row += '&';
+    }
+    for (let m = 0; m <= height - i; m++) {
+      row += '*';
+    }
+    for (let j = 0; j <= height - i; j++) {
+      row += '*';
+    }
+    for (let k = 0; k <= i; k++) {
+      row += '&';
+    }
+    for (let l = 0; l <= i; l++) {
+      row += '&';
+    }
+    for (let m = 0; m <= height - i; m++) {
+      row += '*';
+    }
+    console.log(row);
+  }
 }
 triAngle(12);
