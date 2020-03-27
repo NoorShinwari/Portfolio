4b1a61690e970cfd2f2549e49d46f317b284da2e
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:35:35 +0100

    Exercises on while loop

--- guessGames/loops.js
+++ guessGames/loops.js
@@ -4,92 +4,83 @@
 // the final result was: printing 0, 1, 2
 //HI//
 
-
-/*let i = 0
-while (i < 3) { 
- console.log(i) // for browser use alert() or prompt()... these won't work on node. 
-//or you can use console.log(i++)
- i++
-}*/
+let i = 0;
+while (i < 3) {
+  console.log(i); // for browser use alert() or prompt()... these won't work on node.
+  //or you can use console.log(i++)
+  i++;
+}
 
 // declare a variable i whose value is 3
 // test its value against 0/ check if the number is greater than 0
 // print the variable and decrement it
 
-/*let i = 3
-while (i) { 
- console.log(i) // for browser use alert() or prompt()... these won't work on node. 
- i--
-}*/
+let i = 3;
+while (i) {
+  console.log(i); // for browser use alert() or prompt()... these won't work on node.
+  i--;
+}
 
-/*let i = 9
-while (i<30) { 
- console.log(i) // for browser use alert() or prompt()... these won't work on node. 
-Just experimenting....
- i++
-}*/
+let i = 9;
+while (i < 30) {
+  console.log(i); // for browser use alert() or prompt()... these won't work on node.
+  //Just experimenting....
+  i++;
+}
 
-/*let i = 0
-while (!i) { 
- console.log(i) // for browser use alert() or prompt()... these won't work on node. 
- i++ // it will not execute because 0+1=1 or -1 (i--) is not false means not zero.
-}*/
+let i = 0;
+while (!i) {
+  console.log(i); // for browser use alert() or prompt()... these won't work on node.
+  i++; // it will not execute because 0+1=1 or -1 (i--) is not false means not zero.
+}
 
 /* i want to build a string that looks like this:
 16 times
 "Na Na Na Na ... Batman!"
 the string should be built by looping 16 times,
  and at the end of the loop I want to print the result to the console.*/
- 
- /*x = "NaNaNaNa"
- x4= `${x}${x}${x}${x}` //HaHaHaHa Not a right way
- y = "Batman!"
- let z = x4 + y
- console.log(z)*/
 
-/* let i = 1
- let word = "Na"
- let lastWord = "Batman"
- while (i<16){
-     word+="Na";
-     i++;
-     
- }
- 
- console.log(word + lastWord)*/
+x = 'NaNaNaNa';
+x4 = `${x}${x}${x}${x}`; //HaHaHaHa Not a right way
+y = 'Batman!';
+let z = x4 + y;
+console.log(z);
 
- /*let count = 0
- let strng = "Na "
- let strng2 = "Batman!"
- while (count < 16) {
-    strng += "Na "
-    count++
- }
- console.log(strng+strng2)*/
+let i = 1;
+let word = 'Na';
+let lastWord = 'Batman';
+while (i < 16) {
+  word += 'Na';
+  i++;
+}
 
- /*let currentNumber = 1;
- let sum = 0;
- while (currentNumber<=5) {
-     sum = sum + currentNumber;
-     currentNumber++;
-     console.log(sum)
- }*/
+console.log(word + lastWord);
 
- //string Variables//
+let count = 0;
+let strng = 'Na ';
+let strng2 = 'Batman!';
+while (count < 16) {
+  strng += 'Na ';
+  count++;
+}
+console.log(strng + strng2);
 
- /*
- const NA = "Na ";
- const NA_LENGTH = NA.length;
- const REPEAT_COUNT = 16;
- let str = "";
- while (str.length<REPEAT_COUNT * NA_LENGTH) {
-     str += NA;
- }
-str += "Batman!";
-console.log(str)*/
- 
+let currentNumber = 1;
+let sum = 0;
+while (currentNumber <= 5) {
+  sum = sum + currentNumber;
+  currentNumber++;
+  console.log(sum);
+}
 
- 
- 
+//string Variables//
 
-
+const NA = 'Na ';
+const NA_LENGTH = NA.length;
+const REPEAT_COUNT = 16;
+let str = '';
+while (str.length < REPEAT_COUNT * NA_LENGTH) {
+  str += NA;
+}
+str += 'Batman!';
+console.log(str);
