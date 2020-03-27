16b6863451e021a121ce451301243bb163a67330
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 12:02:04 +0100

    conditional statements

--- javaScript/conditional-statements.js
+++ javaScript/conditional-statements.js
@@ -1,14 +1,13 @@
-let age = 29
+//conditional statements
+
+let age = 29;
 
 if (age < 0) {
-    console.log('Youe weren\'t even born!'); }
-    else if (age < 18){
-        console.log('Too young!')
-    }
-    else if (age > 65) {
-        console.log('Too Old!')
-        
-    }
-    else {console.log('Just the right age!!')}
-
-  
+  console.log("Youe weren't even born!");
+} else if (age < 18) {
+  console.log('Too young!');
+} else if (age > 65) {
+  console.log('Too Old!');
+} else {
+  console.log('Just the right age!!');
+}
