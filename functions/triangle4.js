6da8d0906472ffb2ab3026d0a2ea612c4bc4360b
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:27:25 +0100

    random practice

--- functions/triangle4.js
+++ functions/triangle4.js
@@ -1,49 +1,53 @@
-/*
-function triAngle(height,symbol) {
-    let row ;
-    for (let i = 0; i <= height; i++){
-        row = "";
-        for (let j = 0; j <=(height -i); j++) {
-        row += " ";
-        }*/
-function triAngle(height,symbol) {
-    let row ;
-    for (i = 0; i <= height; i++){
-        row = "";
-        for (j = 0; j <=(height -i); j++) {
-        row += " ";
-        } symb(symbol)
-        } console.log(row);
+function triAngle(height, symbol) {
+  let row;
+  for (let i = 0; i <= height; i++) {
+    row = '';
+    for (let j = 0; j <= height - i; j++) {
+      row += ' ';
+    }
+    function triAngle(height, symbol) {
+      let row;
+      for (i = 0; i <= height; i++) {
+        row = '';
+        for (j = 0; j <= height - i; j++) {
+          row += ' ';
+        }
+        symb(symbol);
+      }
+      console.log(row);
     }
 
     function symb(symbol) {
-        symbol = ""
-        for (let k = 1; k <= i; k++) {
-            row += symbol;
-        }
-        triAngle(15, "**")
-        /*        } console.log(row);
+      symbol = '';
+      for (let k = 1; k <= i; k++) {
+        row += symbol;
+      }
+      triAngle(15, '**');
     }
+    console.log(row);
+  }
 }
-triAngle(15)
-console.log(row);
-//}
-function space() {
-    let  space = " "
-    for (j = 0; j <=(height -i); j++) {
-        row += space;
-    }
 
-    function triAngle(height) {
-        let row ;
-        for (i = 0; i <= height; i++){
-            row = "";
-            for (j = 0; j <=(height -i); j++) {
-                row += " ";
-            } for (k = 1; k <= i; k++) {
-                row += "**";
-            } console.log(row);
-        }
+// triAngle(15)
+// console.log(row);
+// }
+// function space() {
+//     let  space = " "
+//     for (j = 0; j <=(height -i); j++) {
+//         row += space;
+//     }
+
+function triAngle(height) {
+  let row;
+  for (i = 0; i <= height; i++) {
+    row = '';
+    for (j = 0; j <= height - i; j++) {
+      row += ' ';
     }
-    triAngle(15)
-         */
+    for (k = 1; k <= i; k++) {
+      row += '**';
+    }
+    console.log(row);
+  }
+}
+triAngle(15);
