2142df6ede421e47c4645262ded9cd519a3b2cd0
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 20:36:42 +0100

    some correction in the code

--- Callbacks/handlingErrors.js
+++ Callbacks/handlingErrors.js
@@ -1,22 +1,22 @@
 //Handling Errors
 // improved version of loadScript from previous exercises i.e callback.js, callbackInCallback.js
 function loadScript(src, callback) {
-    let script =document.createElement("script";
-    script.src = src;
-    script.onload = () => callback(null,script);
-    script.onerror= () => callback(new Error(`Script load error for ${src} `));
-    document.head.append(script);
+  let script = document.createElement('script');
+  script.src = src;
+  script.onload = () => callback(null, script);
+  script.onerror = () => callback(new Error(`Script load error for ${src} `));
+  document.head.append(script);
 }
 
 // It calls callback(null, script) for successful load and callback(error) otherwise.
 
-loadScript('/my/script.js', function(error,script){
-    if (error) {
-        //handle error
-    } else {
-        //script loaded successfully
-    }
-})
+loadScript('/my/script.js', function(error, script) {
+  if (error) {
+    //handle error
+  } else {
+    //script loaded successfully
+  }
+});
 
 // it is called the "error-first callback" style:
 /**
