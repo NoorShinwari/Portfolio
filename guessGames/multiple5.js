c42a0f52a2b2d097cb42f501e4e518c84d89e363
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:36:11 +0100

    table of 5 using for loop

--- guessGames/multiple5.js
+++ guessGames/multiple5.js
@@ -1,11 +1,12 @@
+//table of five
 const number = 5;
 const multi = ' x ';
 const equal = ' = ';
 //let answer = 55;
 //for (let i = 10; i >=1; i--) {
- //   answer -=5;
- let answer = 0;
- for (let i = 1; i<=10; i++){ 
- answer += 5;
-    console.log(number+multi+i+equal+answer)
+//   answer -=5;
+let answer = 0;
+for (let i = 1; i <= 10; i++) {
+  answer += 5;
+  console.log(number + multi + i + equal + answer);
 }
