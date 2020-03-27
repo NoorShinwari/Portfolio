9b24831cf1680063bdcc501ef3cc1cb92a2cba72
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:46:13 +0100

    converting a number into a string

--- guessGames/table4.js
+++ guessGames/table4.js
@@ -1,18 +1,13 @@
+//converting a number into a string
 let i;
 let j;
-let string = "";
+let string = '';
 let n;
 
-
-
-    string = string.toString();
-    while (string.length < 10){
-        string = "*" + string;  
-        console.log(string)
- 
-    }
-
+string = string.toString();
+while (string.length < 10) {
+  string = '*' + string;
+  console.log(string);
+}
 
 // That is how you convert a number into a string.
-
-    
