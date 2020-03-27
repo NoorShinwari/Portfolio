7b51802af939a6cd19d8ec9941e33d62aac670b5
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:31:49 +0100

    loop exercises prime

--- guessGames/loop-exercises.js
+++ guessGames/loop-exercises.js
@@ -1,34 +1,32 @@
-// n > 1 and (n % n || 1); 
-/*let n = 10
-let i = 2
+// n > 1 and (n % n || 1);
+let n = 10;
+let i = 2;
 for (i = 2; i < n; i++) {
-    let prime = true;
+  let prime = true;
 
-    for (let m =2; m < i-1; m++)  
-    {
-        if (i % m == 0){
-            prime = false;
-        }
+  for (let m = 2; m < i - 1; m++) {
+    if (i % m == 0) {
+      prime = false;
     }
+  }
 
-    if (prime){
-        console.log(i)
-    }
+  if (prime) {
+    console.log(i);
+  }
 }
-*/
-let n = 10
-let i = 2
+
+let n = 10;
+let i = 2;
 for (i = 2; i < n; i++) {
-    let prime = true;
+  let prime = true;
 
-    for (let m =2; m < i-1; m++)  
-    {
-        if (i % m == 0){
-            prime = false;
-        }
+  for (let m = 2; m < i - 1; m++) {
+    if (i % m == 0) {
+      prime = false;
     }
+  }
 
-    if (prime){
-        console.log(i)
-    }
+  if (prime) {
+    console.log(i);
+  }
 }
