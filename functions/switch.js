f02154d2cee908a4a94bc73f3be2edeb352181ce
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:18:30 +0100

    from switch to if exercises

--- functions/switch.js
+++ functions/switch.js
@@ -1,30 +1,34 @@
 // Rewrite the "switch" into an "if"...
-/* Write the code using if..else which would correspong to the following switch:
+//Write the code using if..else which would correspong to the following switch:
 switch (browser) {
-    case "Edge":
-        alert("You've got the Edge!");
-        break;
+  case 'Edge':
+    alert("You've got the Edge!");
+    break;
 
-    case "Chrome":
-    case "Firefox":
-    case "Safari":
-    case "Opera":
-        alert("okay we support these browsers too");
-        break;
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert('okay we support these browsers too');
+    break;
 
-    default:
-        alert("We hope that this page looks ok!")
+  default:
+    alert('We hope that this page looks ok!');
 }
-*/
 
 let browser;
-if (browser == "Edge") {
-    alert("You've got the Edge!")
-//} else if (browser == "Chrome" || "Firefox" || "Safari" || "Opera") { // this is wrong because it means that if browser is equal to chrome or string is true;
-} else if (browser == "Chrome" || browser == "Firefox" || browser == "Safari" || browser == "Opera") { //This is the right way..
-    alert("okay we support these browsers too")
-} else 
-    alert("We hope that this page looks ok!");
+if (browser == 'Edge') {
+  alert("You've got the Edge!");
+  //} else if (browser == "Chrome" || "Firefox" || "Safari" || "Opera") { // this is wrong because it means that if browser is equal to chrome or string is true;
+} else if (
+  browser == 'Chrome' ||
+  browser == 'Firefox' ||
+  browser == 'Safari' ||
+  browser == 'Opera'
+) {
+  //This is the right way..
+  alert('okay we support these browsers too');
+} else alert('We hope that this page looks ok!');
 
 /* Rewrite "if" into "switch"
 
@@ -46,16 +50,13 @@ if (a == 2 || a == 3) {
 
 let a = +prompt('a?', '');
 switch (a) {
-    case 0:
-        alert(0);
-        break;
-    case 1:
-        alert(1);
-        break;
-    case 2:
-    case 3:
-        alert('2,3');
-
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert('2,3');
 }
-
-
