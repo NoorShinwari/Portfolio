38f7ddbf2425e9b2cf27fd0076ccc4f1708b8b26
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 11:41:51 +0100

    making Rectangle using for loop

--- guessGames/rectangle.js
+++ guessGames/rectangle.js
@@ -6,16 +6,15 @@ const COLUMNS = 5;
 // }
 
 // START: build one row
-for (let x = 0; x < ROWS; x++){ 
-    let row = '';
-for (let y = 0; y < COLUMNS; y++){ 
+for (let x = 0; x < ROWS; x++) {
+  let row = '';
+  for (let y = 0; y < COLUMNS; y++) {
     row = row + '*';
-} 
-console.log(row);
+  }
+  console.log(row);
 }
 
 // END
-
 
 // 1. solve for one row
 // 2. repeat to multiple rows
