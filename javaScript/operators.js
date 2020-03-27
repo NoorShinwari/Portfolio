b778f21e57cbad6330b98bd1fcbee69b13982963
NoorShinwari <nanoshin746@gmail.com>
2020-03-27 12:09:10 +0100

    JS operators

--- javaScript/operators.js
+++ javaScript/operators.js
@@ -1,20 +1,19 @@
-const x= 5
-const y = 7
-const z = x - y 
-console.log(z)
+//operators
+const x = 5;
+const y = 7;
+const z = x - y;
+console.log(z);
 
+const a = z;
+const b = x;
+console.log(a * b);
 
-const a = z
-const b = x
-console.log(a*b)
+console.log(9 % 12);
 
-console.log(9 % 12)
-
-const X = 'A'
-const Y = 'B'
-const number = 9
-const remainder = 9 % 3
-console.log(remainder)
-console.log(x<y)
-console.log(x!=X)
-
+const X = 'A';
+const Y = 'B';
+const number = 9;
+const remainder = 9 % 3;
+console.log(remainder);
+console.log(x < y);
+console.log(x != X);
