0fc08edbe10d6932f0fdddddaa349d57fe9c3a32
NoorShinwari <nanoshin746@gmail.com>
2020-03-29 19:08:20 +0200

    promisfy with exercise

--- Promise/promisification.js
+++ Promise/promisification.js
@@ -100,3 +100,31 @@ function promisify(f, manyArgs = false) {
 
 f = promisify(f, true);
 //f(...).then(arrayOfResults => ..., err => ...)
+
+//_________________________________________________________________________________________
+
+function promisify(f, manyArgs = false) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      function callback(err, ...results) {
+        // our custom callback for f
+        if (err) {
+          return reject(err);
+        } else {
+          // resolve with all callback results if manyArgs is specified
+          resolve(manyArgs ? results : results[0]);
+        }
+      }
+
+      args.push(callback);
+
+      console.log('this:', this.toString());
+
+      f.call(this, ...args);
+    });
+  };
+}
+
+function test(a, b) {
+  console.log(`Hello world! a:${a} b:${b}`);
+}
