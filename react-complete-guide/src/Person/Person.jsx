d13e662cbfe1b976bd355d75e159ff97778d690a
NoorShinwari <nanoshin746@gmail.com>
2020-04-03 00:22:05 +0200

    button style with styled-components

--- react-complete-guide/src/Person/Person.jsx
+++ react-complete-guide/src/Person/Person.jsx
@@ -14,11 +14,8 @@ const StyleDiv = styled.div`
   }
 `;
 const person = props => {
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  };
+ 
+  
   return (
     // <div className="Person" style={style}>
     <StyleDiv>
