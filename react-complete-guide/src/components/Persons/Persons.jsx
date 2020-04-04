85148857de2ee58eed963b0117145910363cfba3
NoorShinwari <nanoshin746@gmail.com>
2020-04-04 14:21:32 +0200

    lifecycles hooks for props

--- react-complete-guide/src/components/Persons/Persons.jsx
+++ react-complete-guide/src/components/Persons/Persons.jsx
@@ -3,6 +3,34 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+  static getDerivedStateFromProps(props, state) {
+    console.log('[Persons.jsx] getDerivedStateFromProps');
+    return state;
+  }
+  componentWillReceiveProps(props) {
+    console.log('[Persons.jsx] componentWillRecieveProps', props);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.jsx] shouldComponentUpdate');
+
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Person.jsx] getSnapshotBeforeUpdate');
+    return { messege: 'snapshot!' };
+  }
+
+  // componentWillUpdate() {
+  //   console.log('Person.jsx componentWillUpdate');
+  //}
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Person.jsx] componentDidUpdate');
+    console.log(snapshot);
+  }
+
   render() {
     console.log('[Persons.jsx] rendereing...');
     return this.props.persons.map((person, index) => {
