d0f5ce2f16364635b5950c7ca23778cf9853439f
NoorShinwari <nanoshin746@gmail.com>
2020-04-05 15:07:39 +0200

    Prop Chain

--- react-complete-guide/src/components/Persons/Person/Person.jsx
+++ react-complete-guide/src/components/Persons/Person/Person.jsx
@@ -20,6 +20,7 @@ class Person extends Component {
       // <div className="Person" style={style}>
       // <div className={classes.Person}>
       <Aux>
+        {this.props.isAuth ? <p>Authnticated!</p> : <p>Please Log In!</p>}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -22,6 +22,7 @@ class App extends Component {
     showPersons: false,
     showCockpit: true,
     changeCounter: 0,
+    authenticated: false,
   };
 
   static getDeriveStateFromProps(props, state) {
@@ -80,6 +81,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({ authenticated: true });
+  };
+
   render() {
     console.log('[App.js] render');
     let persons = null;
@@ -90,6 +95,7 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
       );
     }
@@ -108,6 +114,7 @@ class App extends Component {
             showPersons={this.state.showPersons}
             personsLength={this.state.persons.length}
             clicked={this.togglePersonsHandler}
+            login={this.loginHandler}
           />
         ) : null}
         {persons}
