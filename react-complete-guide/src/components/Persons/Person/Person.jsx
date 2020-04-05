6bdf9c60ac95f6a6290483fd4099d77497213a71
NoorShinwari <nanoshin746@gmail.com>
2020-04-05 12:19:09 +0200

    Using PropTypes

--- react-complete-guide/src/components/Persons/Person/Person.jsx
+++ react-complete-guide/src/components/Persons/Person/Person.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/WithClass';
@@ -25,5 +26,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
+};
 
 export default withClass(Person, classes.Person);
