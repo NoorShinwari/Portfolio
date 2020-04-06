be04d533b8a6ba0186e77a42236653a22dd36eab
NoorShinwari <nanoshin746@gmail.com>
2020-04-06 14:57:44 +0200

    Adding a Sidedrawer Toggle Button

--- react-burger-builder/src/components/Layout/Layout.js
+++ react-burger-builder/src/components/Layout/Layout.js
@@ -5,7 +5,14 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
+  };
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer,
+      };
+    });
   };
 
   sideDrawerClosedHandler = () => {
@@ -14,7 +21,7 @@ class Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
