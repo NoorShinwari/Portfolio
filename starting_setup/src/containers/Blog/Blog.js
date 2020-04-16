4db04256497bcfccbb114bfc41a364aa67543dc3
NoorShinwari <nanoshin746@gmail.com>
2020-04-16 19:19:14 +0200

    transforming data

--- starting_setup/src/containers/Blog/Blog.js
+++ starting_setup/src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import post from '../../components/Post/Post';
 
 class Blog extends Component {
   state = {
@@ -11,21 +12,21 @@ class Blog extends Component {
   };
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
-      this.setState({ posts: response.data });
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return { ...post, author: 'Max' };
+      });
+      this.setState({ posts: updatedPosts });
       // console.log(response);
     });
   }
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} />;
+      return <Post key={post.id} title={post.title} author={post.author} />;
     });
     return (
       <div>
-        <section className="Posts">
-          {posts}
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
