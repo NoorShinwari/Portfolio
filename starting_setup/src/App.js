477ccb4be3eee2a186a854355cbbb0ba379b7747
NoorShinwari <nanoshin746@gmail.com>
2020-04-19 21:16:15 +0200

    setting up the router package

--- starting_setup/src/App.js
+++ starting_setup/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import Blog from './containers/Blog/Blog';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
