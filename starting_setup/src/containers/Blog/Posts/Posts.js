8adaba331c7f9e1041312d8536bc36cdbc01a635
NoorShinwari <nanoshin746@gmail.com>
2020-04-20 20:30:57 +0200

    Navigating Programmatically

--- starting_setup/src/containers/Blog/Posts/Posts.js
+++ starting_setup/src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import axios from '../../../axios';
 
 import Post from '../../../components/Post/Post';
@@ -32,7 +32,8 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push({ pathname: '/' + id });
+    //this.props.history.push('/' + id);
   };
 
   render() {
@@ -40,13 +41,14 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map((post) => {
         return (
-          <Link to={'/posts/' + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)}
-            />
-          </Link>
+          //<Link to={'/posts/' + post.id} key={post.id}>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+          // </Link>
         );
       });
     }
