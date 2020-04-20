cecccecef27c1c5c60165902c9c222f8722aa270
NoorShinwari <nanoshin746@gmail.com>
2020-04-20 22:49:28 +0200

    Conditional Redirects

--- starting_setup/src/containers/Blog/NewPost/NewPost.js
+++ starting_setup/src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 import axios from 'axios';
@@ -8,6 +9,7 @@ class NewPost extends Component {
     title: '',
     content: '',
     author: 'Max',
+    submitted: false,
   };
   componentDidMount() {
     console.log(this.props);
@@ -20,12 +22,18 @@ class NewPost extends Component {
     };
     axios.post('/posts', post).then((response) => {
       console.log(response);
+      this.setState({ submitted: true });
     });
   };
 
   render() {
+    let redirect = null;
+    if (this.state.submitted) {
+      redirect = <Redirect to="/posts" />;
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
