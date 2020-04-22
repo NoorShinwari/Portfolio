45528bbc59e47a29ea9cd65e7eec9f09dad49ae1
NoorShinwari <nanoshin746@gmail.com>
2020-04-22 13:00:42 +0200

    Building the checkout container

--- react-burger-builder/src/App.js
+++ react-burger-builder/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containters/BurgerBuilder/BurgerBuilder';
-
+import Checkout from './containters/Checkout/Checkout';
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
