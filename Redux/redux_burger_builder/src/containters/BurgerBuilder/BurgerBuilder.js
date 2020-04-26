538ca4b4ad5f8bb5d6c055bb575f092ef1485e43
NoorShinwari <nanoshin746@gmail.com>
2020-04-26 16:47:29 +0200

    Adjusting Checkout and Contact Data

--- Redux/redux_burger_builder/src/containters/BurgerBuilder/BurgerBuilder.js
+++ Redux/redux_burger_builder/src/containters/BurgerBuilder/BurgerBuilder.js
@@ -53,38 +53,7 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    // // alert('Continue!');
-    // this.setState({ loading: true });
-    // const order = {
-    //   ingredients: this.state.ingredients,
-    //   price: this.state.totalPrice,
-    //   customer: {
-    //     name: 'Noor',
-    //     address: { street: 'Teststreet 1', zipCode: '12345', country: 'Italy' },
-    //     email: 'test@gmail.com',
-    //   },
-    //   deliveryMethod: 'fastest',
-    // };
-    // axios
-    //   .post('/orders.json', order)
-    //   .then((response) => {
-    //     this.setState({ loading: false, purchasing: false });
-    //   })
-    //   .catch((error) => this.setState({ loading: false, purchasing: false }));
-    const queryParams = [];
-    for (let i in this.state.ingredients) {
-      queryParams.push(
-        encodeURIComponent(i) +
-          '=' +
-          encodeURIComponent(this.state.ingredients[i])
-      );
-    }
-    queryParams.push('price=' + this.state.totalPrice);
-    const queryString = queryParams.join('&');
-    this.props.history.push({
-      pathname: '/checkout',
-      search: '?' + queryString,
-    });
+    this.props.history.push('/checkout');
   };
   render() {
     const disabledInfo = {
--- Redux/redux_burger_builder/src/containters/Checkout/ContactData/ContactData.js
+++ Redux/redux_burger_builder/src/containters/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -102,7 +103,7 @@ class ContactData extends Component {
       ].value;
     }
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData,
     };
@@ -189,4 +190,12 @@ class ContactData extends Component {
     );
   }
 }
-export default ContactData;
+
+const mapStateToPtops = (state) => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+  };
+};
+
+export default connect(mapStateToPtops)(ContactData);
