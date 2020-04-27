b322ea6201f8b20ee00119d5050fab0e735f18b3
NoorShinwari <nanoshin746@gmail.com>
2020-04-27 15:58:50 +0200

    Using Utility Functions

--- Redux/redux_start/src/store/reducers/counter.js
+++ Redux/redux_start/src/store/reducers/counter.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-
+import { updateObject } from '../utility';
 const initialState = {
   counter: 0,
 };
@@ -10,28 +10,19 @@ const reducer = (state = initialState, action) => {
       // const newState = Object.assign({}, state);
       // newState.counter = state.counter + 1;
       // return newState;
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
+      // return {
+      //   ...state,
+      //   counter: state.counter + 1,
+      // };
+      return updateObject(state, { counter: state.counter + 1 });
     case actionTypes.DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1,
-      };
+      return updateObject(state, { counter: state.counter - 1 });
     case actionTypes.ADD:
-      return {
-        ...state,
-        counter: state.counter + action.value,
-      };
+      return updateObject(state, { counter: state.counter - action.value });
     case actionTypes.SUBTRACT:
-      return {
-        ...state,
-        counter: state.counter - action.value,
-      };
-    default:
-      return state;
+      return updateObject(state, { counter: state.counter - action.value });
   }
+  return state;
 };
 
 export default reducer;
