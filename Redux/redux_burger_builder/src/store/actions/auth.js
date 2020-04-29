8c857e107a8c430e191242ba03e305c6479fbade
NoorShinwari <nanoshin746@gmail.com>
2020-04-30 00:25:56 +0200

    Getting a Token from the Backend

--- Redux/redux_burger_builder/src/store/actions/auth.js
+++ Redux/redux_burger_builder/src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-
+import axios from 'axios';
 export const authStart = () => {
   return {
     type: actionTypes.AUTH_START,
@@ -23,5 +23,23 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDFw0fkUM_c7wiDg8GR9cO4dTPG67t5Oog',
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
   };
 };
