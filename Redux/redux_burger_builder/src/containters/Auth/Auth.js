9f532f775db2e8b29acdb2ee025aaadf8469d21b
NoorShinwari <nanoshin746@gmail.com>
2020-05-01 03:31:41 +0200

    Validity improvement

--- Redux/redux_burger_builder/src/containters/Auth/Auth.js
+++ Redux/redux_burger_builder/src/containters/Auth/Auth.js
@@ -26,7 +26,7 @@ class Auth extends Component {
       password: {
         elementtype: 'password',
         elementconfig: {
-          type: 'text',
+          type: 'password',
           placeholder: 'Password',
         },
         value: '',
@@ -49,15 +49,32 @@ class Auth extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
     if (rules.required) {
       isValid = value.trim() !== '' && isValid;
     }
+
     if (rules.minLength) {
       isValid = value.length >= rules.minLength && isValid;
     }
+
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
     }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
     return isValid;
   }
 
