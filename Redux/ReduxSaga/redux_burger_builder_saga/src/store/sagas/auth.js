c9569be7021bb22193e90025de3e415c491985fb
NoorShinwari <nanoshin746@gmail.com>
2020-05-05 00:24:23 +0200

    Hooking the Saga Up (to the Store and Actions)

--- Redux/ReduxSaga/redux_burger_builder_saga/src/store/sagas/auth.js
+++ Redux/ReduxSaga/redux_burger_builder_saga/src/store/sagas/auth.js
@@ -1,7 +1,7 @@
 import { put } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 
-function* logout(action) {
+export function* logoutSaga(action) {
   yield localStorage.removeItem('token');
   yield localStorage.removeItem('expiratinDate');
   yield localStorage.removeItem('userId');
