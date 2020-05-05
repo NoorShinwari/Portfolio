4d6de0c12c73b183c7af70a0885a5bf619bdcf6c
NoorShinwari <nanoshin746@gmail.com>
2020-05-05 20:27:10 +0200

    Moving the Orders Side Effects into Sagas

--- Redux/ReduxSaga/redux_burger_builder_saga/src/store/actions/index.js
+++ Redux/ReduxSaga/redux_burger_builder_saga/src/store/actions/index.js
@@ -5,7 +5,17 @@ export {
   setIngredients,
   fetchIngredientsFailed,
 } from './burgerBuilder';
-export { purchaseBurger, purchaseInit, fetchOrders } from './order';
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  purchaseBurgerStart,
+  purchaseBurgerFail,
+  purchaseBurgerSuccess,
+  fetchOrderSuccess,
+  fetchOrderStart,
+  fetchOrderFail,
+} from './order';
 export {
   auth,
   logout,
