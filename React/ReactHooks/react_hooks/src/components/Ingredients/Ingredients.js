34380ddaad4b28928abe01a064fb7f818da6aa9b
NoorShinwari <nanoshin746@gmail.com>
2020-05-06 14:37:59 +0200

    Deleting Ingredients

--- React/ReactHooks/react_hooks/src/components/Ingredients/Ingredients.js
+++ React/ReactHooks/react_hooks/src/components/Ingredients/Ingredients.js
@@ -33,9 +33,16 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = (ingredientId) => {
-    setUserIngredients((prevIngredients) =>
-      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-    );
+    fetch(
+      `https://react-hooks-update-fb720.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: 'DELETE',
+      }
+    ).then((response) => {
+      setUserIngredients((prevIngredients) =>
+        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+      );
+    });
   };
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngredients(filteredIngredients);
