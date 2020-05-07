a40756bb53f1e7f24ab8e8b745de047156cd7d9c
NoorShinwari <nanoshin746@gmail.com>
2020-05-08 01:21:05 +0200

    Finishing the Store Hook

--- React/ReactHooks/replace_redux_with_hooks/src/hooks-store/store.js
+++ React/ReactHooks/replace_redux_with_hooks/src/hooks-store/store.js
@@ -6,8 +6,17 @@ let listeners = [];
 
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
   const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
 
   useEffect(() => {
     listeners.push(setState);
@@ -15,4 +24,13 @@ const useStore = () => {
       listeners = listeners.filter((li) => li !== setState);
     };
   }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
 };
