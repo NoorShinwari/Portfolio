20a5ebe3b03ca7faaf47e3975d25aedb44df7590
NoorShinwari <nanoshin746@gmail.com>
2020-05-18 05:14:59 +0200

    Mapping Lists to Widgets

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/flutter_quiz_app/lib/answer.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/flutter_quiz_app/lib/answer.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
-  Answer(this.selectHandler);
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
-        child: Text('Answer 1'),
+        textColor: Colors.white,
+        child: Text(answerText),
         color: Colors.blue,
         onPressed: selectHandler,
       ),
