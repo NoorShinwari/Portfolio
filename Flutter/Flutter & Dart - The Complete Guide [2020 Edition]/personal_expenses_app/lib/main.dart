db8f9dba27b914e46f22b66cbabe4ed9b1b28094
NoorShinwari <nanoshin746@gmail.com>
2020-05-19 00:16:49 +0200

    Using String Interpolation

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/personal_expenses_app/lib/main.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/personal_expenses_app/lib/main.dart
@@ -68,7 +68,7 @@ class MyHomePage extends StatelessWidget {
                         10,
                       ),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
