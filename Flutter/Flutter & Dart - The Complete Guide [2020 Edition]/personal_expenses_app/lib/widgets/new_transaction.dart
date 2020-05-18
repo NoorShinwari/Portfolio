198bc18e4eb5e91b01765603275578ff0ad4e69d
NoorShinwari <nanoshin746@gmail.com>
2020-05-19 01:36:27 +0200

    Adding User Transactions to the List

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/personal_expenses_app/lib/widgets/new_transaction.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/personal_expenses_app/lib/widgets/new_transaction.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
+  final Function addTx;
+
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +36,12 @@ class NewTransaction extends StatelessWidget {
               // onChanged: (value) => amountInput = value,
             ),
             FlatButton(
-              onPressed: () {},
+              onPressed: () {
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
               child: Text(
                 'Add Transaction',
               ),
