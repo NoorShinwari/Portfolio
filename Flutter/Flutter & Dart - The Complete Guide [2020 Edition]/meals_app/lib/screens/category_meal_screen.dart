facab15a365bae74ec3240690ef2b8944690b2c4
NoorShinwari <nanoshin746@gmail.com>
2020-05-22 00:59:34 +0200

    Displaying Recipe Items & Using Network Images

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/meals_app/lib/screens/category_meal_screen.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/meals_app/lib/screens/category_meal_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './dummy_data.dart';
+import '../widgets/meal_item.dart';
+
+import '../dummy_data.dart';
 
 class CategoryMealScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -24,7 +26,12 @@ class CategoryMealScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
         },
         itemCount: categoryMeals.length,
       ),
