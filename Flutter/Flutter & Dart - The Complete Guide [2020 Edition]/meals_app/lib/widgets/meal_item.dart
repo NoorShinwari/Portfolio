c10992dc11237279b41ef5cad93271c08cb695b0
NoorShinwari <nanoshin746@gmail.com>
2020-05-22 06:43:18 +0200

    Popping Pages & Passing Data Back

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/meals_app/lib/widgets/meal_item.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/meals_app/lib/widgets/meal_item.dart
@@ -10,14 +10,17 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
-  MealItem(
-      {@required this.id,
-      @required this.title,
-      @required this.imageUrl,
-      @required this.duration,
-      @required this.complexity,
-      @required this.affordability});
+  MealItem({
+    @required this.id,
+    @required this.title,
+    @required this.imageUrl,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+    @required this.removeItem,
+  });
 
   String get complexityText {
     // if (complexity == Complexity.Simple) {
@@ -61,10 +64,16 @@ class MealItem extends StatelessWidget {
   }
 
   void selectMeal(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed(
+    Navigator.of(ctx)
+        .pushNamed(
       MealDetailScreen.routeName,
       arguments: id,
-    );
+    )
+        .then((result) {
+      if (result != null) {
+        removeItem(result);
+      }
+    });
   }
 
   @override
