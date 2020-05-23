95c2d28ea4df83a4551b6fe63c314272f57541c7
NoorShinwari <nanoshin746@gmail.com>
2020-05-23 07:51:40 +0200

    Providing an Orders Object

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/main.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './providers/orders_provider.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products_provider.dart';
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => Cart(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
+        )
       ],
       child: MaterialApp(
         title: 'MyShop',
