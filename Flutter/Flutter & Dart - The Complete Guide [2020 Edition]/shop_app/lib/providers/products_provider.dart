08716202e85731c17a356e6613fda50ed62c52fa
NoorShinwari <nanoshin746@gmail.com>
2020-05-25 01:15:39 +0200

    http package and Sending POST Requests

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/providers/products_provider.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/providers/products_provider.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product_provider.dart';
 
@@ -67,6 +70,17 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    const url = 'https://shop-app-flutter-udemy.firebaseio.com/products.json';
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    );
     final newProduct = Product(
         id: DateTime.now().toString(),
         title: product.title,
