7bc4d9b1c5018bc0c8569ca6bd013fe2395e690d
NoorShinwari <nanoshin746@gmail.com>
2020-05-24 02:46:51 +0200

    Edit Products Screen&Using Forms & Working with Form Inputs

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/screens/user_product_screen.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/screens/user_product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../screens/edit_product_screen.dart';
 import '../widgets/app_drawer.dart';
 import '../providers/products_provider.dart';
 import '../widgets/user_product_item.dart';
@@ -17,7 +18,7 @@ class UserProductScreen extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              //...
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
             },
           )
         ],
