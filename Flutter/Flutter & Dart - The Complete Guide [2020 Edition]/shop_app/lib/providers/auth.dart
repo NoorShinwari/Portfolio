ee3d2a02b61094e86b2da4f4345a0593a0f01810
NoorShinwari <nanoshin746@gmail.com>
2020-05-27 01:44:04 +0200

    Allowing Users to Log  In

---
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/providers/auth.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCAuZ0JxmWDwQJn5fbF44-_6RlQF085PRI';
+
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+
+  Future<void> signIn(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+}
