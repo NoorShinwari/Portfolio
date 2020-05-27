1002f50912ef9c01d7700c95537b68153743e597
NoorShinwari <nanoshin746@gmail.com>
2020-05-27 20:05:16 +0200

    Connecting the "Favorite" Status to Users

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/providers/products.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/providers/products.dart
@@ -43,7 +43,8 @@ class Products with ChangeNotifier {
   ];
   // var _showFavoritesOnly = false;
   final String authToken;
-  Products(this.authToken, this._items);
+  final String userId;
+  Products(this.authToken, this.userId, this._items);
 
   List<Product> get items {
     // if (_showFavoritesOnly) {
@@ -71,7 +72,7 @@ class Products with ChangeNotifier {
   // }
 
   Future<void> fetchAndSetProducts() async {
-    final url =
+    var url =
         'https://shop-app-flutter-udemy.firebaseio.com/products.json?auth=$authToken';
     try {
       final response = await http.get(url);
@@ -79,6 +80,10 @@ class Products with ChangeNotifier {
       if (extractedData == null) {
         return;
       }
+      url =
+          'https://shop-app-flutter-udemy.firebaseio.com/userFavorites/$userId.json?auth=$authToken';
+      final favoriteResponse = await http.get(url);
+      final favoriteData = json.decode(favoriteResponse.body);
       final List<Product> loadedProducts = [];
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
@@ -86,7 +91,8 @@ class Products with ChangeNotifier {
           title: prodData['title'],
           description: prodData['description'],
           price: prodData['price'],
-          isFavorite: prodData['isFavorite'],
+          isFavorite:
+              favoriteData == null ? false : favoriteData[prodId] ?? false,
           imageUrl: prodData['imageUrl'],
         ));
       });
@@ -108,7 +114,6 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
-          'isFavorite': product.isFavorite,
         }),
       );
       final newProduct = Product(
