d1ca3d21e14b7d5923c7cf40d705227062d05bb7
NoorShinwari <nanoshin746@gmail.com>
2020-05-27 02:12:33 +0200

    Handling Authentication Errors

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/providers/auth.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/providers/auth.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -12,18 +14,25 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCAuZ0JxmWDwQJn5fbF44-_6RlQF085PRI';
-
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      // print(json.decode(response.body));
+      final responseData = json.decode((response.body));
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw (error);
+    }
   }
 
   Future<void> signIn(String email, String password) async {
