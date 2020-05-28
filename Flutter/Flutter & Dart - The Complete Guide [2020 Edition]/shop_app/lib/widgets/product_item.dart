f6d87cc59786b87b1de47c5eebc8d376be002461
NoorShinwari <nanoshin746@gmail.com>
2020-05-28 12:49:17 +0200

    Adding a Hero Transition

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/widgets/product_item.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/shop_app/lib/widgets/product_item.dart
@@ -28,10 +28,13 @@ class ProductItem extends StatelessWidget {
               arguments: product.id,
             );
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/placeholder.png'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/placeholder.png'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         footer: GridTileBar(
