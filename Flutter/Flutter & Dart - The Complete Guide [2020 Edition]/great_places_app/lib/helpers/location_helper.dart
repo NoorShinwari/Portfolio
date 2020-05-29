f244f41c65275c2b54f5b0d0b89d549cfb9d6a6c
NoorShinwari <nanoshin746@gmail.com>
2020-05-29 04:48:14 +0200

    Added an API instead of GOOGLE API

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/great_places_app/lib/helpers/location_helper.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/great_places_app/lib/helpers/location_helper.dart
@@ -1,8 +1,8 @@
-const GOOGLE_API_KEY = 'I DONT HAVE A CREDIT CARD';
+const MAPQUEST_API_KEY = 'aKkcteP94KOzNkNIJWmDpynKT26XiQqZ';
 
 class locationHelper {
   static String generateLocationPreviewImage(
       {double latitude, double longitude}) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+    return 'https://www.mapquestapi.com/staticmap/v5/map?key=$MAPQUEST_API_KEY&center=$latitude,$longitude&zoom=10&type=hyb&size=600,400@2x';
   }
 }
