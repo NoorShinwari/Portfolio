203f5c098b0b27b42900b9533b55ff9175569b0a
NoorShinwari <nanoshin746@gmail.com>
2020-05-30 23:41:10 +0200

    Improvement in Location map

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/great_places_app/lib/helpers/location_helper.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/great_places_app/lib/helpers/location_helper.dart
@@ -3,6 +3,6 @@ const MAPQUEST_API_KEY = 'aKkcteP94KOzNkNIJWmDpynKT26XiQqZ';
 class LocationHelper {
   static String generateLocationPreviewImage(
       {double latitude, double longitude}) {
-    return 'https://www.mapquestapi.com/staticmap/v5/map?key=$MAPQUEST_API_KEY&center=$latitude,$longitude&zoom=10&type=hyb&size=600,400@2x';
+    return 'https://www.mapquestapi.com/staticmap/v5/map?key=$MAPQUEST_API_KEY&center=$latitude,$longitude&locations=$latitude,$longitude&zoom=12&type=hyb&size=600,400@2x';
   }
 }
