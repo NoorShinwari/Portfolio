9022f6f862fa07a07fbe4fee1a633dc07fe6bb6d
NoorShinwari <nanoshin746@gmail.com>
2020-06-02 03:06:39 +0200

    Storing Extra User Data

--- Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/chat_app_2/lib/widgets/auth/auth_form.dart
+++ Flutter/Flutter & Dart - The Complete Guide [2020 Edition]/chat_app_2/lib/widgets/auth/auth_form.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  AuthForm(this.submitFn);
+  AuthForm(this.submitFn, this.isLoading);
+  final bool isLoading;
   final void Function(
     String email,
     String password,
@@ -100,21 +101,24 @@ class _AuthFormState extends State<AuthForm> {
                   SizedBox(
                     height: 12,
                   ),
-                  RaisedButton(
-                    child: Text(_isLogin ? 'Login' : 'Signup'),
-                    onPressed: _trySubmit,
-                  ),
-                  FlatButton(
-                    child: Text(_isLogin
-                        ? 'Create new account'
-                        : 'I already have an account'),
-                    onPressed: () {
-                      setState(() {
-                        _isLogin = !_isLogin;
-                      });
-                    },
-                    textColor: Theme.of(context).primaryColor,
-                  ),
+                  if (widget.isLoading) CircularProgressIndicator(),
+                  if (!widget.isLoading)
+                    RaisedButton(
+                      child: Text(_isLogin ? 'Login' : 'Signup'),
+                      onPressed: _trySubmit,
+                    ),
+                  if (!widget.isLoading)
+                    FlatButton(
+                      child: Text(_isLogin
+                          ? 'Create new account'
+                          : 'I already have an account'),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                      textColor: Theme.of(context).primaryColor,
+                    ),
                 ],
               ),
             ),
